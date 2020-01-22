22d41b4e97f6b47ec0bc365060d476463a1c8b4c
shendezhi02 <shendezhi02@countrygarden.com.cn>
2020-01-22 15:17:21 +0800

    feat(tree):增加树控件

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,80 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <tree :data="treeData" @change="nodeValueChange"></tree>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from "./components/HelloWorld.vue";
+import Tree from "./components/AvgTree.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Tree
+  },
+  data() {
+    return {
+      treeData: {
+        id: 0,
+        label: "XXX科技有限公司",
+        children: [
+          {
+            id: 2,
+            label: "产品研发部",
+            children: [
+              {
+                id: 5,
+                label: "研发-前端"
+              },
+              {
+                id: 6,
+                label: "研发-后端"
+              },
+              {
+                id: 9,
+                label: "UI设计"
+              },
+              {
+                id: 10,
+                label: "产品经理"
+              }
+            ]
+          },
+          {
+            id: 3,
+            label: "销售部",
+            children: [
+              {
+                id: 7,
+                label: "销售一部"
+              },
+              {
+                id: 8,
+                label: "销售二部"
+              }
+            ]
+          },
+          {
+            id: 4,
+            label: "财务部"
+          },
+          {
+            id: 11,
+            label: "HR人事"
+          }
+        ]
+      }
+    };
+  },
+  methods: {
+    nodeValueChange: function() {}
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
+  },
---
+++ src/components/AvgTree.vue
@@ -0,0 +1,22 @@
+<template>
+  <!-- <el-tree :data="data" default-expand-all></el-tree> -->
+  <org-tree :data="data"></org-tree>
+</template>
+<script>
+import OrgTree from "vue2-org-tree";
+export default {
+  name: "AvgTree",
+  props: {
+    data: Object
+  },
+  components: { "org-tree": OrgTree },
+  data() {
+    return {};
+  }
+};
+</script>
+<style>
+.el-tree-node {
+  float: left;
+}
+</style>
